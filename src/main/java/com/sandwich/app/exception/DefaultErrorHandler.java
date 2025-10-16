cb067cda574264bd7217dc6495efff10f4e5cb33
AnVoronov <AnVoronov@croc.ru>
2025-10-16 19:51:14 +0300

    Исправлены ошибки

--- src/main/java/com/sandwich/app/exception/DefaultErrorHandler.java
+++ src/main/java/com/sandwich/app/exception/DefaultErrorHandler.java
@@ -1,15 +1,17 @@
 package com.sandwich.app.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class DefaultErrorHandler {
 
     @ExceptionHandler
     public AppExceptionResponse defaultHandler(Throwable exception) {
+        log.error(exception.getMessage(), exception);
         return new AppExceptionResponse()
             .setStatus(HttpStatus.BAD_REQUEST)
             .setErrorMessage(exception.getMessage());
